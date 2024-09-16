428299e5f7cf8acfee8c433066bf277b722b0e8a
Mona Tagelsir Sharif <91035196+monatagelsir7@users.noreply.github.com>
2024-09-16 01:50:20 +0000

    update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -19,7 +20,11 @@ def generate_viz_diamonds(save_as_image=True):
     """Generates and optionally saves the diamond price distribution plot."""
     plt.subplots(figsize=(20, 8))
     palette = ["#c94727","#ea5b17","#e57716","#f2a324","#a2c0a6","#7ac0a8","#5e9786","#557260","#5b5572"]
-    p = sns.histplot(dataset["price"],color=palette[8],kde=True,bins=30,alpha=1,fill=True,edgecolor="black",linewidth=3)
+    
+    # Ensure 'price' is a 1D array
+    prices = np.array(dataset["price"]).flatten()
+    
+    p = sns.histplot(prices, color=palette[8], kde=True, bins=30, alpha=1, fill=True, edgecolor="black", linewidth=3)
     p.axes.lines[0].set_color("orange")
     p.axes.set_title("\nDiamond's Price Distribution\n", fontsize=25)
     plt.ylabel("Count", fontsize=20)
